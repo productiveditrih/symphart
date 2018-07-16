a00c5f23dc5929d8512059037d23b94150207299
productiveditrih <productiveditrih@gmail.com>
2018-07-16 23:17:32 +0200

    Init

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+	namespace App\Controller;
+
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+	class ArticleController extends Controller {
+		/**
+		 * @Route("/")
+		 * @Method({"GET", "POST"})
+		 */
+		public function index(){
+			// return new Response('<html><body>Hello</body></html>');
+
+			$articles= ['Article 1', 'Article 2'];
+
+			return $this->render('articles/index.html.twig', array('articles' => $articles));
+
+
+		}
+	}
